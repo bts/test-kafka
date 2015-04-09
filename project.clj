1324c88534750dce1d0fb667c6f3939a82c72e79
Brian Schroeder <bts@gmail.com>
2015-04-09 11:36:30 -0400

    Send log4j to slfj4 outside dev

--- project.clj
+++ project.clj
@@ -8,19 +8,16 @@
                                com.sun.jdmk/jmxtools
                                javax.jms/jms
                                junit
-                               log4j
                                org.slf4j/slf4j-log4j12]]
                  [org.apache.kafka/kafka_2.10 "0.8.2.1"]
                  [commons-io/commons-io "2.4"]
-                 [com.101tec/zkclient "0.3"
-                  :exclusions [log4j]]]
+                 [com.101tec/zkclient "0.3"]
+                 ;; send kafka/zk log4j logging to slf4j
+                 [org.slf4j/slf4j-api "1.7.12"]
+                 [org.slf4j/log4j-over-slf4j "1.7.12"]]
+  :exclusions [log4j org.slf4j/slf4j-api]
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.7.0-alpha5"]
-                                  [zookeeper-clj "0.9.3"
-                                   :exclusions [log4j]]
-                                  ;; send zookeeper-clj logging to slf4j:
-                                  [org.slf4j/slf4j-api "1.7.12"]
-                                  [org.slf4j/log4j-over-slf4j "1.7.12"]
+                                  [zookeeper-clj "0.9.3"]
+                                  [org.apache.kafka/kafka-clients "0.8.2.1"]
                                   ;[org.slf4j/slf4j-simple "1.7.12"]
-                                  [org.slf4j/slf4j-nop "1.7.12"]
-                                  [org.apache.kafka/kafka-clients "0.8.2.1"]]
-                   :exclusions [org.slf4j/slf4j-api]}})
+                                  [org.slf4j/slf4j-nop "1.7.12"]]}})
